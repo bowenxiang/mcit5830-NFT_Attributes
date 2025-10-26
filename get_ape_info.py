a921a52b02832dcaddc2a203e2745217d152d52e
bowenxiang <55109815+bowenxiang@users.noreply.github.com>
2025-10-26 11:37:44 -0400

    2nd fix

--- get_ape_info.py
+++ get_ape_info.py
@@ -15,7 +15,7 @@ with open('ape_abi.json', 'r') as f:
 
 ############################
 # Connect to an Ethereum node
-api_url = "https://cloudflare-eth.com" 
+api_url = "https://eth-mainnet.g.alchemy.com/v2/WMoV3ya-pyB8BLRr2aPrG"  # YOU WILL NEED TO PROVIDE THE URL OF AN ETHEREUM NODE
 provider = HTTPProvider(api_url)
 web3 = Web3(provider)
 
@@ -28,43 +28,43 @@ def get_ape_info(ape_id):
     data = {'owner': "", 'image': "", 'eyes': ""}
 
     # YOUR CODE HERE
-    try:
-        # 1. Set up the contract instance
-        contract = web3.eth.contract(address=contract_address, abi=abi)
-        
-        # 2. Call the contract to get the owner
-        data['owner'] = contract.functions.ownerOf(ape_id).call()
-        
-        # 3. Call the contract to get the tokenURI
-        token_uri = contract.functions.tokenURI(ape_id).call()
-        
-        # 4. Convert the IPFS URI to an HTTP Gateway URL
-        # We use the Pinata gateway as suggested: https://gateway.pinata.cloud/ipfs/
-        # The token_uri is in the format "ipfs://HASH/ID"
-        ipfs_hash_path = token_uri.split('//')[-1]
-        
-        metadata_url = f"https://gateway.pinata.cloud/ipfs/{ipfs_hash_path}"
-        
-        # 5. Fetch the metadata from the IPFS gateway
-        response = requests.get(metadata_url)
-        response.raise_for_status()  # Raise an error for bad responses
-        metadata = response.json()
-        
-        # 6. Extract the image URI from the metadata
-        data['image'] = metadata.get('image', '')
-        
-        # 7. Extract the "Eyes" attribute from the metadata
-        attributes = metadata.get('attributes', [])
-        for attr in attributes:
-            if attr.get('trait_type') == 'Eyes':
-                data['eyes'] = attr.get('value', '')
-                break  # Stop looping once we've found the 'Eyes' trait
-
-    except Exception as e:
-        print(f"An error occurred while fetching info for ape {ape_id}: {e}")
-        # If an error occurs, the function will return the default 'data' dict with empty strings
-
-    # END YOUR CODE
+    
+    # Step 1: Create contract instance
+    contract = web3.eth.contract(address=contract_address, abi=abi)
+    
+    # Step 2: Get the owner of the NFT
+    owner = contract.functions.ownerOf(ape_id).call()
+    data['owner'] = owner
+    
+    # Step 3: Get the tokenURI
+    token_uri = contract.functions.tokenURI(ape_id).call()
+    
+    # Step 4: Convert IPFS URI to HTTP gateway URL
+    # tokenURI returns something like: ipfs://QmeSjSinHpPnmXmspMjwiXyN6zS4E9zccariGR3jxcaWtq/1
+    if token_uri.startswith('ipfs://'):
+        # Remove 'ipfs://' prefix
+        ipfs_hash = token_uri.replace('ipfs://', '')
+        # Use Pinata gateway with API key
+        metadata_url = f"https://gateway.pinata.cloud/ipfs/{ipfs_hash}"
+    else:
+        metadata_url = token_uri
+    
+    # Step 5: Fetch metadata from IPFS
+    headers = {
+        'pinata_api_key': '3ba5786bb6e8e2619f14'
+    }
+    response = requests.get(metadata_url, headers=headers)
+    metadata = response.json()
+    
+    # Step 6: Extract image URI
+    data['image'] = metadata['image']
+    
+    # Step 7: Extract eyes attribute from attributes list
+    attributes = metadata.get('attributes', [])
+    for attribute in attributes:
+        if attribute.get('trait_type') == 'Eyes':
+            data['eyes'] = attribute.get('value')
+            break
 
     assert isinstance(data, dict), f'get_ape_info{ape_id} should return a dict'
     assert all([a in data.keys() for a in
